2c45593c7351a33f3ced0e878aaad8906c1e0b07
IvaFil <filipovichiv@yandex.ru>
2022-09-29 10:17:23 +0300

    Задача 54

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp14
             int[,] array = new int[3, 4];
 
 
+
             for (int i = 0; i < array.GetLength(0); i++)
             {
                     for (int j = 0; j < array.GetLength(1); j++)

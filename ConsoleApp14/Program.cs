c98a90ff010a87c1bff5f98093fe06bea0c2e674
IvaFil <filipovichiv@yandex.ru>
2022-09-29 10:16:19 +0300

    Задача 54

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -10,6 +10,46 @@ namespace ConsoleApp14
     {
         static void Main(string[] args)
         {
+            int[,] array = new int[3, 4];
+
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = new Random().Next(1, 10);
+                    }
+            }
+
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array.GetLength(1) - 1; k++)
+                    {
+                        if (array[i, k] < array[i, k + 1])
+                        {
+                            int temp = array[i, k + 1];
+                            array[i, k + 1] = array[i, k];
+                            array[i, k] = temp;
+                        }
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+
+
         }
     }
 }
